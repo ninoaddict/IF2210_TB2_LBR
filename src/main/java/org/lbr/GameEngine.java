4f330641bf19fc0995a9edbaf747febf94c0aaf1
Berto Richardo Togatorop <bertotogatorop27@gmail.com>
2024-05-21 23:57:07 +0700

    feat: add more implementation for GameEngine and BearAttack
    Co-authored-by: scifo04 <scifohutahaean@gmail.com>
    Co-authored-by: Bbennn <sihombingbenjamin2004@gmail.com>

--- src/main/java/org/lbr/GameEngine.java
+++ src/main/java/org/lbr/GameEngine.java
@@ -1,104 +1,166 @@
-//package org.lbr;
-//
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//
-//public class GameEngine {
-//    private Player[] currPlayer;
-//    private int currState;
-//    private HashMap<String,GameObject> cardKeys;
-//
-//    public GameEngine() {
-//        currPlayer = new Player[2];
-//        currState = 0;
-//        currPlayer[0] = new Player(new ArrayList<>());
-//        currPlayer[1] = new Player(new ArrayList<>());
-//        cardKeys.put("Hiu Darat",new Carnivore("Hiu Darat",));
-//        cardKeys.add("Sapi");
-//        cardKeys.add("Domba");
-//        cardKeys.add("Kuda");
-//        cardKeys.add("Ayam");
-//        cardKeys.add("Beruang");
-//        cardKeys.add("Biji Jagung");
-//        cardKeys.add("Biji Labu");
-//        cardKeys.add("Biji Stroberi");
-//        cardKeys.add("Sirip Hiu");
-//        cardKeys.add("Susu");
-//        cardKeys.add("Daging Domba");
-//        cardKeys.add("Daging Kuda");
-//        cardKeys.add("Telur");
-//        cardKeys.add("Daging Beruang");
-//        cardKeys.add("Jagung");
-//        cardKeys.add("Labu");
-//        cardKeys.add("Stroberi");
-//        cardKeys.add("Accelerate");
-//        cardKeys.add("Delay");
-//        cardKeys.add("Instant Harvest");
-//        cardKeys.add("Destroy");
-//        cardKeys.add("Protect");
-//        cardKeys.add("Trap");
-//    }
-//
-//    public int getState() {
-//        return currState;
-//    }
-//
-//    public void switchState() {
-//        currState = currState ^ 1;
-//    }
-//
-//    // method
-//
-//}
-//package org.lbr;
-//
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//
-//public class GameEngine {
-//    private Player[] currPlayer;
-//    private int currState;
-//    private HashMap<String,GameObject> cardKeys;
-//
-//    public GameEngine() {
-//        currPlayer = new Player[2];
-//        currState = 0;
-//        currPlayer[0] = new Player(new ArrayList<>());
-//        currPlayer[1] = new Player(new ArrayList<>());
-//        cardKeys.put("Hiu Darat",new Carnivore("Hiu Darat",));
-//        cardKeys.add("Sapi");
-//        cardKeys.add("Domba");
-//        cardKeys.add("Kuda");
-//        cardKeys.add("Ayam");
-//        cardKeys.add("Beruang");
-//        cardKeys.add("Biji Jagung");
-//        cardKeys.add("Biji Labu");
-//        cardKeys.add("Biji Stroberi");
-//        cardKeys.add("Sirip Hiu");
-//        cardKeys.add("Susu");
-//        cardKeys.add("Daging Domba");
-//        cardKeys.add("Daging Kuda");
-//        cardKeys.add("Telur");
-//        cardKeys.add("Daging Beruang");
-//        cardKeys.add("Jagung");
-//        cardKeys.add("Labu");
-//        cardKeys.add("Stroberi");
-//        cardKeys.add("Accelerate");
-//        cardKeys.add("Delay");
-//        cardKeys.add("Instant Harvest");
-//        cardKeys.add("Destroy");
-//        cardKeys.add("Protect");
-//        cardKeys.add("Trap");
-//    }
-//
-//    public int getState() {
-//        return currState;
-//    }
-//
-//    public void switchState() {
-//        currState = currState ^ 1;
-//    }
-//
-//    // method
-//
-//}
+package org.lbr;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class GameEngine {
+    private Player[] currPlayer;
+    private int currTurn;
+    private ArrayList<String> cardKeys;
+    private String winner;
+    private ScheduledExecutorService timerService;
+
+    private static final ArrayList<String> CARD_KEYS = new ArrayList<>();
+
+    static {
+        // 0 - 5 : Animal
+        CARD_KEYS.add("HIU_DARAT");
+        CARD_KEYS.add("SAPI");
+        CARD_KEYS.add("DOMBA");
+        CARD_KEYS.add("KUDA");
+        CARD_KEYS.add("AYAM");
+        CARD_KEYS.add("BERUANG");
+
+        // 6 - 9 : Plant
+        CARD_KEYS.add("BIJI_JAGUNG");
+        CARD_KEYS.add("BIJI_LABU");
+        CARD_KEYS.add("BIJI_STROBERI");
+
+        // 10 - 14 : Item
+        CARD_KEYS.add("ACCELERATE");
+        CARD_KEYS.add("DELAY");
+        CARD_KEYS.add("INSTANT_HARVEST");
+        CARD_KEYS.add("DESTROY");
+        CARD_KEYS.add("PROTECT");
+        CARD_KEYS.add("TRAP");
+
+        // 15 - 23 : Product
+        CARD_KEYS.add("SIRIP_HIU");
+        CARD_KEYS.add("SUSU");
+        CARD_KEYS.add("DAGING_DOMBA");
+        CARD_KEYS.add("DAGING_KUDA");
+        CARD_KEYS.add("TELUR");
+        CARD_KEYS.add("DAGING_BERUANG");
+        CARD_KEYS.add("JAGUNG");
+        CARD_KEYS.add("LABU");
+        CARD_KEYS.add("STROBERI");
+    }
+
+    public GameEngine() {
+        currPlayer = new Player[2];
+        currTurn = 1;
+        currPlayer[0] = new Player(new ArrayList<>());
+        currPlayer[1] = new Player(new ArrayList<>());
+        timerService = Executors.newScheduledThreadPool(1);
+    }
+
+    // getter
+    public int getCurrTurn() {
+        return currTurn;
+    }
+
+    public void nextTurn() {
+        currTurn++;
+        if (currTurn >= 20) {
+            winner = getWinner();
+        }
+        run();
+    }
+
+    public int getCurrPlayer() {
+        return (currTurn % 2) - 1;
+    }
+
+    public String getCardKey(int idx) {
+        return cardKeys.get(idx);
+    }
+
+    // method
+    public GameObject constructGameObject(int idx) {
+        String name = getCardKey(idx);
+        if (idx <= 5) {
+            if (idx == 0) {
+                return new Carnivore(name);
+            } else if (idx <= 3) {
+                return new Herbivore(name);
+            } else {
+                return new Omnivore(name);
+            }
+        } else if (idx <= 8) {
+            return new Plant(name);
+        } else if (idx <= 14) {
+            return switch (idx) {
+                case 9 -> new Accelerate();
+                case 10 -> new Delay();
+                case 11 -> new InstantHarvest();
+                case 12 -> new Destroy();
+                case 13 -> new Protect();
+                default -> new Trap();
+            };
+        } else {
+            return new Product(name);
+        }
+    }
+
+    public ArrayList<GameObject> getShuffleCards() {
+        ArrayList<GameObject> gameObjects = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 4; i++) {
+            int randomInt = random.nextInt(15); // 0 to 14
+            gameObjects.add(constructGameObject(randomInt));
+        }
+        return gameObjects;
+    }
+
+    public String getWinner() {
+        if (currPlayer[0].getGulden() > currPlayer[1].getGulden()) {
+            return "Player 1";
+        } else if (currPlayer[0].getGulden() < currPlayer[1].getGulden()) {
+            return "Draw";
+        } else {
+            return "Player 2";
+        }
+    }
+
+    public void defaultSetup() {
+        // Initialize or reset game state if needed
+
+    }
+
+    public void WaitParalelly(int seconds) {
+        timerService.scheduleAtFixedRate(new Runnable() {
+            int remainingTime = seconds;
+
+            @Override
+            public void run() {
+                if (remainingTime > 0) {
+                    System.out.println("Time left: " + remainingTime + " seconds");
+                    remainingTime--;
+                } else {
+                    timerService.shutdown();
+                }
+            }
+        }, 0, 1, TimeUnit.SECONDS);
+    }
+
+    public void run() {
+        defaultSetup();
+        while (currTurn < 20) {
+            ArrayList<GameObject> shuffles = getShuffleCards();
+            // ADD TO CURRENT PLAYER
+            int bearAttackChance = (new Random()).nextInt(100) + 1;
+            if (bearAttackChance <= 60) {
+                BearAttack.refresh(currPlayer[getCurrPlayer()]);
+                Random random = new Random();
+                WaitParalelly( random.nextInt(31) + 30);
+                BearAttack.execute();
+            }
+            nextTurn();
+        }
+        timerService.shutdown();
+    }
+}
--- src/main/java/org/lbr/Shop.java
+++ src/main/java/org/lbr/Shop.java
@@ -3,36 +3,31 @@ package org.lbr;
 import java.util.Map;
 
 public class Shop {
-    private Map<String, Integer> products;
+    private Map<Product, Integer> products;
 
-    public Shop(Map<String, Integer> products) {
+    public Shop(Map<Product, Integer> products) {
         this.products = products;
     }
 
     // getter
-    public Map<String, Integer> getProducts() {
+    public Map<Product, Integer> getProducts() {
         return products;
     }
 
     // setter
-    public void setItems(Map<String, Integer> products) {
+    public void setItems(Map<Product, Integer> products) {
         this.products = products;
     }
 
     // method
-    public void reduceProduct(String key){
+    public void reduceProduct(Product key){
         products.put(key, products.get(key) - 1);
+        if (products.get(key) == 0){
+            products.remove(key);
+        }
     }
 
-    public void reduceProduct(Product product){
-        reduceProduct(product.getName());
-    }
-
-    public void addProduct(String name){
-        products.put(name, products.getOrDefault(name, 0) + 1);
-    }
-
-    public void addProduct(Product product){
-        addProduct(product.getName());
+    public void addProduct(Product newProduct){
+        products.put(newProduct, products.getOrDefault(newProduct, 0) + 1);
     }
 }
